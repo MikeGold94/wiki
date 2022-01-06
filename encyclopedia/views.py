fd964420847981d54c372579704228f06d72cc7f
mikegold <mike.goldes@gmail.com>
2022-01-06 09:58:39 -0800

    Added functionality: entry page, index page

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from markdown2 import Markdown
 
 from . import util
 
@@ -8,3 +9,16 @@ def index(request):
         "entries": util.list_entries()
     })
 
+
+def wikientry(request, title):
+    markdowner = Markdown()
+    wiki_entry = util.get_entry(title)
+    if wiki_entry is None:
+        return render(request, "encyclopedia/entrymissing.html", {
+            "title": title
+        })
+    else:
+        return render(request, "encyclopedia/wikientry.html", {
+            "entryAsHTML": markdowner.convert(wiki_entry),
+            "title": title
+        })
